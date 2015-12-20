05b7ec2220ce6df176ef2f3a8c049bc647f955d5
adrianrol <adrianrol@live.com.mx>
2015-12-19 23:37:58 -0600

    Initial Commit

---
+++ 4Reglas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4Reglas
+//
+//  Created by Adrian Rodríguez Lloréns on 19/12/15.
+//  Copyright © 2015 adappsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
